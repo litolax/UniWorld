8d8043cdc600adb5de3e8ebb07500fb0543a2c78
Yuri Ratkevich <ura_rat@mail.ru>
2023-10-02 10:38:40 +0300

    Add mongodb

---
+++ src/server/database.ts
@@ -0,0 +1,43 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env.local' });
+
+const MONGODB_URI = process.env.MONGODB_URI ?? '';
+const MONGODB_DB = process.env.DB_NAME ?? '';
+const dev = process.env.NODE_ENV !== 'production';
+
+if (!MONGODB_URI) {
+	throw new Error('Define the MONGODB_URI environmental variable');
+}
+
+if (!MONGODB_DB) {
+	throw new Error('Define the MONGODB_DB environmental variable');
+}
+
+let cachedClient: any = null;
+let cachedDb: any = null;
+
+export async function connectToDatabase() {
+	if (cachedClient && cachedDb) {
+		return {
+			client: cachedClient,
+			db: cachedDb
+		};
+	}
+
+	let client = new MongoClient(
+		dev
+			? `${process.env.MONGODB_URI}`
+			: `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI}`);
+	await client.connect();
+	let db = client.db(MONGODB_DB);
+
+	cachedClient = client;
+	cachedDb = db;
+
+	return {
+		client: cachedClient,
+		db: cachedDb
+	};
+}
