92ba3466a840ea8def40a870971c75999adb2cfc
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-03-20 12:56:32 +0300

    feat: add mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,17 @@
 import '../styles/globals.css';
+import '@mantine/core/styles.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { appWithTranslation } from 'next-i18next';
-import { ConfigProvider, theme } from 'antd';
+import { MantineProvider } from '@mantine/core';
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
 	return (
-		<ConfigProvider
-			theme={{
-				algorithm: theme.darkAlgorithm
-			}}
-		>
+		<MantineProvider defaultColorScheme="dark">
 			<SessionProvider session={session}>
 				<Component {...pageProps} />
 			</SessionProvider>
-		</ConfigProvider>
+		</MantineProvider>
 	);
 };
 
