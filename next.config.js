1bff7abf0787a38629fb89a1497bb7346daae120
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-04-09 19:46:29 +0300

    chore: update deps

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { i18n } = require('./next-i18next.config')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  i18n
+  i18n,
+  outputFileTracing: false,
+  localePath: path.resolve('./public/locales'),
 }
 
+// eslint-disable-next-line no-undef
 module.exports = nextConfig
