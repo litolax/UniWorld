301899696252316ab3c92abc255b2f5e6ad0bd47
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-04-11 12:18:11 +0300

    chore: remove unused methods

--- src/server/database.ts
+++ src/server/database.ts
@@ -1,6 +1,5 @@
-import { MongoClient, ObjectId } from 'mongodb'
+import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
-import { TAccount } from '../types/TAccount'
 
 dotenv.config({ path: './.env.local' })
 
@@ -42,25 +41,4 @@ export async function connectToDatabase() {
     client: cachedClient,
     db: cachedDb,
   }
-}
-
-export async function validateAccount(username: string): Promise<boolean> {
-  const { db } = await connectToDatabase()
-  const collection = await db.collection('accounts')
-
-  const result = (await collection.find({ username: username }).toArray()) as TAccount[]
-
-  return result.length > 0
-}
-
-export async function createAccount(username: string, email: string) {
-  const user: TAccount = {
-    _id: new ObjectId(),
-    username: username,
-    email: email,
-    points: 0,
-  }
-
-  const { db } = await connectToDatabase()
-  await db.collection('accounts').insertOne(user)
 }
