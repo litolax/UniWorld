f76f04672afe09c807f6e2fbd0fa7691bde3e81c
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-05-07 22:59:24 +0300

    feat: generatePassword function

--- src/utils.ts
+++ src/utils.ts
@@ -76,3 +76,15 @@ export const dataFix = (obj: any) => {
   })
   return newObj
 }
+
+export const generatePassword = (length: number): string => {
+  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+'
+  let password = ''
+
+  for (let i = 0; i < length; ++i) {
+    const randomIndex = Math.floor(Math.random() * charset.length)
+    password += charset[randomIndex]
+  }
+
+  return password
+}
