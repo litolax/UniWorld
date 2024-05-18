add5330518c630437fd9ba558bc19d0902348de4
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-05-18 15:46:04 +0300

    feat: add chunk function

--- src/utils.ts
+++ src/utils.ts
@@ -88,3 +88,12 @@ export const generatePassword = (length: number): string => {
 
   return password
 }
+
+export const chunk = <T>(array: T[], size: number): T[][] => {
+  if (!array.length) {
+    return []
+  }
+  const head = array.slice(0, size)
+  const tail = array.slice(size)
+  return [head, ...chunk(tail, size)]
+}
