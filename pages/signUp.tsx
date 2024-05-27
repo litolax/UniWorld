2b3ededc84a5fd5f997248dec07a6990ab33fc31
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-05-27 20:25:53 +0300

    ci: build fix

--- pages/signUp.tsx
+++ pages/signUp.tsx
@@ -11,8 +11,6 @@ import {
   Title,
 } from '@mantine/core'
 import classes from '../styles/AuthenticationTitle.module.css'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
 import { useRouter } from 'next/navigation'
 import { useForm } from '@mantine/form'
 import { GetServerSideProps } from 'next'
