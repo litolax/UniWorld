ead8813fae09da57155a97a68cffffbffea5c99b
Yuri Ratkevich <ura_rat@mail.ru>
2024-06-27 00:02:06 +0300

    fix: about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,8 @@
 import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
-import { authRedirect } from '../src/server/authRedirect'
 import { Container, Paper, Text, Title } from '@mantine/core'
 import Wrapper from '../components/Wrapper'
-import { getSession } from 'next-auth/react'
-import { getAccountByEmail } from '../src/server/account'
-import { TAccount } from '../src/types/TAccount'
 
 export default function About() {
   const { t } = useTranslation('feedback')
@@ -33,16 +29,8 @@ export default function About() {
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const session = await getSession(ctx)
-  let currentAccount
-  if (session && session.user?.email) {
-    currentAccount = JSON.parse(
-      JSON.stringify(await getAccountByEmail(session.user?.email)),
-    ) as TAccount
-  }
-  const locale = currentAccount ? currentAccount.locale : ctx.locale ? ctx.locale : 'ru'
+  const locale = ctx.locale ? ctx.locale : 'ru'
   return {
-    redirect: await authRedirect(ctx),
     props: {
       ...(await serverSideTranslations(locale, ['common', 'errors'])),
     },
